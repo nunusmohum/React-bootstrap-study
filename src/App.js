f75cf7ddf7e1d6be1a8c3e3d016aee9d5c100843
nunusmohum <halojw301@gmail.com>
2022-07-23 07:00:17 +0900

    React-bootstrap 설치

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { Button, Navbar, Nav, Container } from "react-bootstrap";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#Cart">Cart</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div className="main-bg"></div>
+    </div>
+  );
 }
 
 export default App;
